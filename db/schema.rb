55047ce4b8b73cc06d2e41ffb6fec7c48e258a08
Bo Butcher <cbbutcher91@gmail.com>
2016-05-12 16:57:59 -0400

    Added two new models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512145751) do
+ActiveRecord::Schema.define(version: 20160512205044) do
+
+  create_table "authors", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +31,11 @@ ActiveRecord::Schema.define(version: 20160512145751) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "book_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
