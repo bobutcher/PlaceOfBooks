6800b1a917aaf49fb2a64afad1f91f4bd95f5ecf
Bo Butcher <cbbutcher91@gmail.com>
2016-05-13 11:26:37 -0400

    Added functionality in controllers

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,22 +1,41 @@
 class AuthorsController < ApplicationController
   def index
+    @authors = Authors.all
   end
 
   def new
+    @author = Authors.new
   end
 
   def show
+    @author = Author.find(params[:id])
   end
 
   def edit
-  end
-
-  def create
-  end
-
-  def destroy
+    @author = Author.find(params[:id])
   end
 
   def update
+    @author = Author.find(params[:id])
+    @author.update(author_params)
+    redirect_to action: "show", id: @author.id
+  end
+
+  def create
+    @author = Author.create(author)
+    redirect_to action: "show", id: @author.id
+
+  end
+
+  def destroy
+    @author = Author.find(params[:id]).destroy
+    redirect_to action: "index"
+  end
+
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name, :bio)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,39 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def destroy
+    @user = User.find(params[:id]).destroy
+    redirect_to action: "index"
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to action: "show", id: @user.id
   end
 
   def create
+    @user = User.create(user_params)
+    redirect_to action: "show", id: @user.id
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
